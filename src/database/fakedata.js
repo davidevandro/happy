03e01adebe70e4a03c254a6d372dd70529d4e2bb
David Martins <davidevandro@gmail.com>
2020-10-16 22:06:41 -0300

    Configurando o servidor

---
+++ src/database/fakedata.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        id: 1,
+        lat: -22.9697343,
+        lng: -43.1848931, 
+        name: "Lar das Meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de riscou e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1600711725042-deb9fbaeb1e3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1597992350431-56cb7e28a7a2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instruction: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+   
+    },
+    {
+        id: 2,
+        lat: -22.9697343,
+        lng: -43.1948931, 
+        name: "Lar dos Meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de riscou e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1600711725042-deb9fbaeb1e3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1597992350431-56cb7e28a7a2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instruction: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"     
+    }
+]
---
+++ src/server.js
@@ -0,0 +1,23 @@
+//importar dependencia do pacote
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js')
+
+//iniciando o express
+const server = express()
+server
+    //utilizando os arquivos estaticos
+    .use(express.static('public'))
+
+    //configurar template engine
+    .set('views', path.join(__dirname, "views"))
+    .set('view engine', 'hbs')
+
+    //rotas da aplicaca
+    .get('/',pages.index)
+    .get('/orphanage',pages.orphanage)
+    .get('/orphanages',pages.orphanages)
+    .get('/create-orphanage',pages.createOrphanage)
+
+//ligar o servidor
+server.listen(5500)
